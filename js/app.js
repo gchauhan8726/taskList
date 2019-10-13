1cef244ae341285723b2f9421adff26243a2db46
Gaurav Chauhan <gaurav.c@opensenselabs.com>
2019-10-13 23:37:42 +0530

    Adding taskList functionality with localStorage.

--- js/app.js
+++ js/app.js
@@ -7,10 +7,31 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners() {
+    document.addEventListener('DOMContentLoaded', getTasks);
     form.addEventListener('submit', addTask);
     taskList.addEventListener('click', removeTask);
     clearBtn.addEventListener('click', clearTask);
     filter.addEventListener('keyup', filterTask);
+}
+
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task) {
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+        li.appendChild(document.createTextNode(task));
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
 }
 
 function addTask(e) {
@@ -26,23 +47,63 @@ function addTask(e) {
     link.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(link);
     taskList.appendChild(li);
+
+    storeTaskInLocalStorge(taskInput.value);
+
     taskInput.value = '';
     e.preventDefault();
+}
+
+function storeTaskInLocalStorge(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function removeTask(e) {
     if(e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are you sure?')){
             e.target.parentElement.parentElement.remove();
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }    
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index) {
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function clearTask(e) {
     // taskList.innerHTML = '';
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild);
+        clearTaskFromLocalStorage();
     }
+}
+
+function clearTaskFromLocalStorage() {
+    localStorage.clear();
 }
 
 function filterTask(e) {
